238e35fa7970b30af16ea6dcadde04ffacf92c84
sanket2323 <sanketmane2323@gmail.com>
2026-05-09 19:35:30 +1000

    used super keyword for task 2 implementation

--- a2.py
+++ a2.py
@@ -1,6 +1,7 @@
 # DO NOT modify or add any import statements
 from support import *
 
+
 # Name:Sanket Mane
 # Student Number: 50040467
 # Favorite Box: 
@@ -26,10 +27,11 @@ class Tile:
     def is_blocking(self) -> bool:
         return False
 
-#task 2
+
+# task 2
 class Floor(Tile):
     def __init__(self):
-        pass
+        super().__init__()
 
     def __repr__(self) -> str:
         return "Floor()"
@@ -39,6 +41,24 @@ class Floor(Tile):
 
     def get_type(self) -> str:
         return FLOOR
+
+
+# task 3
+class Wall(Tile):
+    def __init__(self):
+        pass
+
+    def __repr__(self) -> str:
+        return "Wall()"
+
+    def __str__(self) -> str:
+        return WALL
+
+    def get_type(self) -> str:
+        return WALL
+
+    def is_blocking(self) -> bool:
+        return True
 
 
 def main() -> None:
